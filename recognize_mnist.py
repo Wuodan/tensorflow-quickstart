8a7785896bbb95b11056c3ce9d8487ecc7fe0acf
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2023-12-20 02:45:58 +0100

    next version of 'scan all MNIST images', still fails

--- recognize_mnist.py
+++ recognize_mnist.py
@@ -5,17 +5,12 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import argparse
 import tensorflow as tf
 from PIL import Image
-
-import glob
-import re
+import numpy as np
 
 # Recognize a digit from a variable-sized input image
-def recognize_digit(image_path, model):
+def recognize_digit(image, model):
     # Load and preprocess the input image
-    input_image = Image.open(image_path)
-
-    input_image_array = tf.keras.preprocessing.image.img_to_array(input_image)
-    input_image_array = input_image_array / 255.0  # Normalize to [0, 1]
+    input_image_array = image / 255.0  # Normalize to [0, 1]
 
     # Make predictions using the pre-trained model
     predictions = model.predict(tf.expand_dims(input_image_array, axis=0), verbose=0)
@@ -26,7 +21,7 @@ def recognize_digit(image_path, model):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Recognize a digit from a variable-sized input image.")
-    parser.add_argument("--mnist_folder", type=str, required=True, help="Path to the MNIST dataset folder")
+    parser.add_argument("--mnist_file", type=str, required=True, help="Path to the MNIST dataset file (mnist.npz)")
     args = parser.parse_args()
 
     # Load the pre-trained models
@@ -35,10 +30,17 @@ if __name__ == "__main__":
         model_name = pattern.search(file_name).group(1)
         model = tf.keras.models.load_model(file_name)
 
-        # Loop over all images in the MNIST dataset folder
-        for image_file in glob.glob(os.path.join(args.mnist_folder, '*.png')):
-            # Perform digit recognition
-            recognized_digit = recognize_digit(image_file, model)
+        # Load MNIST dataset
+        mnist_data = np.load(args.mnist_file)
+        x_test, y_test = mnist_data['x_test'], mnist_data['y_test']
 
-            # Print the recognized digit and image file name
-            print(f"Recognized digit: {recognized_digit}, model-name: {model_name}, image: {image_file}")
+        # Loop over all images in the MNIST dataset
+        for i in range(len(x_test)):
+            image = x_test[i]
+
+            # Perform digit recognition
+            recognized_digit = recognize_digit(image, model)
+
+            # Print the recognized digit and true label
+            true_label = y_test[i]
+            print(f"Recognized digit: {recognized_digit}, True label: {true_label}, model-name: {model_name}")
