849bacf71c5b474859392adc82fa53ae5ff6e371
Stefan Kuhn <stefan.kuhn.zurich@gmail.com>
2023-12-20 03:05:19 +0100

    next version of 'scan all MNIST images', first output

--- recognize_mnist.py
+++ recognize_mnist.py
@@ -23,27 +23,34 @@ def recognize_digit(image, model):
     return recognized_digit
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Recognize a digit from a variable-sized input image.")
+    parser = argparse.ArgumentParser(description="Recognize digits from the MNIST dataset using pre-trained models.")
     parser.add_argument("--mnist_file", type=str, required=True, help="Path to the MNIST dataset file (mnist.npz)")
     args = parser.parse_args()
 
     # Load the pre-trained models
     pattern = re.compile(r'trained_model_(.+)\.h5')
-    for file_name in glob.glob('trained_model_*.h5'):
-        model_name = pattern.search(file_name).group(1)
-        model = tf.keras.models.load_model(file_name)
 
-        # Load MNIST dataset
-        mnist_data = np.load(args.mnist_file)
-        x_test, y_test = mnist_data['x_test'], mnist_data['y_test']
+    # Load MNIST dataset
+    mnist_data = np.load(args.mnist_file)
+    x_test, y_test = mnist_data['x_test'], mnist_data['y_test']
 
-        # Loop over all images in the MNIST dataset
-        for i in range(len(x_test)):
+    # Loop over all unique labels in the MNIST dataset
+    unique_labels = set(y_test)
+    for true_label in unique_labels:
+        # Filter images with the current label
+        label_indices = np.where(y_test == true_label)[0]
+
+        # Loop over images with the current label
+        for i in label_indices:
             image = x_test[i]
 
-            # Perform digit recognition
-            recognized_digit = recognize_digit(image, model)
+            # Loop over all models
+            for file_name in glob.glob('trained_model_*.h5'):
+                model_name = pattern.search(file_name).group(1)
+                model = tf.keras.models.load_model(file_name)
 
-            # Print the recognized digit and true label
-            true_label = y_test[i]
-            print(f"Recognized digit: {recognized_digit}, True label: {true_label}, model-name: {model_name}")
+                # Perform digit recognition
+                recognized_digit = recognize_digit(image, model)
+
+                # Print the recognized digit, true label, model name, and index
+                print(f"True label: {true_label}, Recognized digit: {recognized_digit}, model-name: {model_name}, Index: {i}")
